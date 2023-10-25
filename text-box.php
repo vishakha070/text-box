92ca3b5b3b4052d3359aa55fa0c0df85dc7bd050
“Vishakha <vigupta@gainsight.com>
2023-10-25 12:38:23 +0530

    added vscode config

--- text-box.php
+++ text-box.php
@@ -1,11 +1,12 @@
 <?php
+
 /**
  * Plugin Name:       Text Box
  * Description:       A Box of Text.
  * Requires at least: 5.7
  * Requires PHP:      7.0
  * Version:           0.1.0
- * Author:            Ali Alaa
+ * Author:            vishakha070
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       text-box
@@ -20,7 +21,8 @@
  *
  * @see https://developer.wordpress.org/block-editor/tutorials/block-tutorial/writing-your-first-block-type/
  */
-function blocks_course_text_box_block_init() {
-	register_block_type_from_metadata( __DIR__ );
+function blocks_course_text_box_block_init()
+{
+	register_block_type_from_metadata(__DIR__);
 }
-add_action( 'init', 'blocks_course_text_box_block_init' );
+add_action('init', 'blocks_course_text_box_block_init');
